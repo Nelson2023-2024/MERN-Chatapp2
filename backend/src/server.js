6dae5901e80a2bbbcf3f98e9d2c865a094b98346
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-11-25 01:12:45 +0300

    Configured cloudinary

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import { configDotenv } from "dotenv";
 import express from "express";
-import cookieParse from "cookie-parse"
+import cookieParse from "cookie-parser"
 import { authRoutes } from "../routes/auth.route.js";
 import { connectToMongoDB } from "../lib/db/connectToMongoDB.js";
 configDotenv()
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8000
 
 app.use(express.json())
-app.use(cookieParse)
+app.use(cookieParse())
 
 app.use('/api/auth', authRoutes)
 
