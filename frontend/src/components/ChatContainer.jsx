b6460e3f23d7855f755e4f2477dcc33241a09b0b
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-12-02 20:38:39 +0300

    Messages skeleton completed

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageImput from "./MessageImput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
   const { getMessages, messages, isMessagesLoading, selectedUser } =
@@ -10,7 +11,11 @@ const ChatContainer = () => {
   useEffect(() => {
     getMessages(selectedUser._id); //get messages between us and selected user
   }, [selectedUser._id, getMessages]);
-  if (isMessagesLoading) return <div>Loading.....</div>;
+  if (isMessagesLoading) return <div className="flex-1 flex flex-col overflow-auto">
+    <ChatHeader/>
+    <MessageSkeleton/>
+    <MessageImput/>
+  </div>;
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
