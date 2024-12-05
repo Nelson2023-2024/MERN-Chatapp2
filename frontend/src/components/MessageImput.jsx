2c9846cb7ef8d79f21fe971f87c594248814932f
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-12-05 12:51:25 +0300

    Send Message functionality completed

--- frontend/src/components/MessageImput.jsx
+++ frontend/src/components/MessageImput.jsx
@@ -1,8 +1,121 @@
+import { useRef, useState } from "react";
+import { Image, Send, X } from "lucide-react";
+import { useChatStore } from "../store/useChatStore";
+import toast from "react-hot-toast";
 
 const MessageImput = () => {
-  return (
-    <div>MessageImput</div>
-  )
-}
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
 
-export default MessageImput
+  const { sendMessage } = useChatStore();
+
+  const handleImageChange = (e) => {
+    // get the files that user selected
+    const file = e.target.files[0];
+
+    //if type of the file is not an image
+    if (!file.type.startsWith("image/")) {
+      toast.error("Please select an image file");
+      return;
+    }
+
+    //if user selected an image
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const removeImage = () => {
+    setImagePreview(null);
+    if (fileInputRef.current) fileInputRef.current.value = "";
+  };
+
+  const handlesendMessage = async (e) => {
+    e.preventDefault();
+
+    //if the input is empty and no image is selected
+    if (!text.trim() && !imagePreview) return;
+
+    //but if the message exists
+    try {
+      await sendMessage({
+        text: text.trim(),
+        image: imagePreview,
+      });
+
+      //vlear form
+      setText("");
+      setImagePreview(null);
+      if (fileInputRef.current) fileInputRef.current.value = "";
+    } catch (error) {
+      toast.error("Error sending the message");
+      console.error("Failed to send message", error);
+    }
+  };
+
+  return (
+    <div className="p-4 w-full">
+      {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+
+      <form onSubmit={handlesendMessage} className="flex items-center gap-2">
+        <div className="flex-1 flex gap-2">
+          <input
+            type="text"
+            className="w-full input input-bordered rounded-lg input-sm sm:input-md"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          {/* to select image from our machine */}
+          <input
+            type="file"
+            accept="image/*"
+            className="hidden"
+            ref={fileInputRef}
+            onChange={handleImageChange}
+          />
+
+          <button
+            type="button"
+            className={`hidden sm:flex btn btn-circle ${
+              imagePreview ? "text-emerald-500" : "text-zinc-400"
+            }`}
+            onClick={() => fileInputRef.current?.click()}
+          >
+            <Image size={20} />
+          </button>
+        </div>
+        <button
+          type="button"
+          className={`btn btn-sm btn-circle `}
+          disabled={!text.trim() && !imagePreview}
+        >
+          <Send size={22} />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MessageImput;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -31,6 +31,23 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  //sendMessage function
+  sendMessage: async (messageData) => {
+    const { messages, selectedUser } = get();
+
+    try {
+      const response = await axiosInstance.post(
+        `/message/send/${selectedUser._id}`,
+        messageData
+      );
+      if(response) toast.success("Sent successfully")
+      set({ messages: [...messages, response.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+      toast.error("Error sending the message");
     }
   },
 
