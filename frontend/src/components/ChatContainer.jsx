637baaf5b684ca3a8ae9ad50828f732caf49a25b
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-12-06 21:39:19 +0300

    Updated the scrollinto view when a new message is sent

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -7,26 +7,38 @@ import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { getMessages, messages, isMessagesLoading, selectedUser, listenToIncomingMessages, unListenToIncomingMessages } =
-    useChatStore();
+  const {
+    getMessages,
+    messages,
+    isMessagesLoading,
+    selectedUser,
+    listenToIncomingMessages,
+    unListenToIncomingMessages,
+  } = useChatStore();
 
   const { authUser } = useAuthStore();
-
 
   console.log(messages);
 
   useEffect(() => {
     getMessages(selectedUser._id); //get messages between us and selected user
-    listenToIncomingMessages()
+    listenToIncomingMessages();
 
-    return () => unListenToIncomingMessages()
-  }, [selectedUser._id, getMessages, listenToIncomingMessages, unListenToIncomingMessages]);
+    return () => unListenToIncomingMessages();
+  }, [
+    selectedUser._id,
+    getMessages,
+    listenToIncomingMessages,
+    unListenToIncomingMessages,
+  ]);
 
-  const lastMessageRef = useRef(null) // reference for the last message
+  const lastMessageRef = useRef(null); // reference for the last message
 
-  useEffect(() =>{
-    if(messages.length > 0) lastMessageRef.current?.scrollIntoView({behavior: "smooth"})
-  },[messages])
+  useEffect(() => {
+    if (messages.length > 0)
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   if (isMessagesLoading)
     return (
       <div className="flex-1 flex flex-col overflow-auto">
@@ -40,10 +52,10 @@ const ChatContainer = () => {
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message,index) => (
+        {messages.map((message, index) => (
           <div
             key={message._id}
-            ref={index === messages.length - 1 ? lastMessageRef : null}
+            ref={lastMessageRef}
             className={`chat ${
               message.senderId === authUser._id ? "chat-end" : "chat-start"
             }`}
@@ -60,9 +72,7 @@ const ChatContainer = () => {
                 />
               </div>
             </div>
-            <div className="chat-header mb-1">
-             
-            </div>
+            <div className="chat-header mb-1"></div>
             <div className="chat-bubble rounded-md flex flex-col">
               {/* if there is an image for this message display it */}
               {message.Image && (
@@ -75,8 +85,8 @@ const ChatContainer = () => {
               {message.text && <p>{message.text}</p>}
             </div>
             <time className="text-xs opacity-50 ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
+              {formatMessageTime(message.createdAt)}
+            </time>
           </div>
         ))}
       </div>
